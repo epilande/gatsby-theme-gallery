f06c549bebd7e0e7db5c686310691fac5bd0bd0b
Emmanuel Pilande <epilande@gmail.com>
2019-07-30 13:53:45 -0700

    :building_construction: Create gallery directory if it doens't exists

--- gatsby-theme-gallery/gatsby-node.js
+++ gatsby-theme-gallery/gatsby-node.js
@@ -1,8 +1,21 @@
+const fs = require("fs");
 const path = require("path");
+const mkdirp = require("mkdirp");
 
-exports.createPages = ({ actions }, options) => {
+// Ensure that content directory exist at the site-level
+exports.onPreBootstrap = ({ reporter, store }, themeOptions) => {
+  const { program } = store.getState();
+  const galleryPath = themeOptions.galleryPath || "content/gallery";
+  const dir = path.join(program.directory, galleryPath);
+  if (!fs.existsSync(dir)) {
+    reporter.info(`Creating the "${dir}" directory`);
+    mkdirp.sync(dir);
+  }
+};
+
+exports.createPages = ({ actions }, themeOptions) => {
   const { createPage } = actions;
-  const { basePath } = options;
+  const { basePath } = themeOptions;
 
   createPage({
     path: basePath || "/",
