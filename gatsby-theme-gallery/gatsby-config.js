373ebda3d451ccfaf3f000d6364ee202865130c5
Emmanuel Pilande <epilande@gmail.com>
2019-07-30 13:52:39 -0700

    :wrench: Set up source filesystem & sharp plugin

--- gatsby-theme-gallery/gatsby-config.js
+++ gatsby-theme-gallery/gatsby-config.js
@@ -1,3 +1,18 @@
-module.exports = () => ({
-  plugins: ["gatsby-plugin-typescript"],
-});
+module.exports = options => {
+  const { galleryPath = "content/gallery" } = options;
+
+  return {
+    plugins: [
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: galleryPath,
+          path: galleryPath,
+        },
+      },
+      "gatsby-plugin-sharp",
+      "gatsby-transformer-sharp",
+      "gatsby-plugin-typescript",
+    ],
+  };
+};
