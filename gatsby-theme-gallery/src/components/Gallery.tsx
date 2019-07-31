3172fd9df0baa8e6537d8fd12c6cefd231cb5c19
Emmanuel Pilande <epilande@gmail.com>
2019-07-30 21:27:41 -0700

    :sparkles: Set up Gallery with grid tiles

--- gatsby-theme-gallery/src/components/Gallery.tsx
+++ gatsby-theme-gallery/src/components/Gallery.tsx
@@ -1,17 +1,41 @@
-import * as React from "react";
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+import styled from "@emotion/styled";
 import Img from "gatsby-image";
 import useGallery from "../hooks/useGallery";
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
+  grid-auto-rows: 15rem;
+  grid-gap: 0.5rem;
+`;
+
+const Tile = styled.div`
+  position: relative;
+`;
 
 const Gallery = () => {
   const images = useGallery();
 
   return (
-    <div>
-      <h2>Gallery</h2>
+    <GridContainer>
       {images.map(image => (
-        <Img key={image.id} alt={image.name} fluid={image.fluid} />
+        <Tile key={image.id}>
+          <Img
+            alt={image.name}
+            fluid={image.fluid}
+            style={{
+              position: "absolute",
+              left: 0,
+              top: 0,
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </Tile>
       ))}
-    </div>
+    </GridContainer>
   );
 };
 
