df573ad4fcf2e886af3f5e1600f5c1dbb04aac19
Emmanuel Pilande <epilande@gmail.com>
2019-07-31 08:49:30 -0700

    :iphone: Make it responsive

--- gatsby-theme-gallery/src/components/Grid.ts
+++ gatsby-theme-gallery/src/components/Grid.ts
@@ -2,9 +2,14 @@ import styled from "@emotion/styled";
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
-  grid-auto-rows: 15rem;
+  grid-template-columns: repeat(auto-fit, minmax(16rem, 1fr));
+  grid-auto-rows: 16rem;
   grid-gap: 0.5rem;
+
+  @media (min-width: 1280px) {
+    grid-template-columns: repeat(auto-fit, minmax(24rem, 1fr));
+    grid-auto-rows: 24rem;
+  }
 `;
 
 export default Grid;
