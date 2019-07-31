7e4b83dd4ecb365d413466f61a1d0030dde2892b
Emmanuel Pilande <epilande@gmail.com>
2019-07-30 21:44:22 -0700

    :bathtub: Clean up

--- gatsby-theme-gallery/src/pages/index.tsx
+++ gatsby-theme-gallery/src/pages/index.tsx
@@ -1,15 +1,10 @@
 import * as React from "react";
-import { Link } from "gatsby";
 import Gallery from "../components/Gallery";
 
 const HomePage = () => {
   return (
     <div>
-      <h1>
-        <pre>Hello, gatsby-theme-gallery</pre>
-      </h1>
       <Gallery />
-      <Link to="/">Back</Link>
     </div>
   );
 };
